62d087d03288a45b10ae347785e18936b11f2805
nlutala <nathanlutala@outlook.com>
2025-05-12 13:01:22 +0100

    Change the display of the root and start implementing the functionality of adding different categories

--- personal_expense_tracker/main.py
+++ personal_expense_tracker/main.py
@@ -5,11 +5,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# Get the categories and the spending budget for categories
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {
+        "Subscriptions": 50,
+        "Groceries": 200,
+        "Entertainment": 100,
+        "Transportation": 150,
+    }
 
-
-@app.get("/items/{item_id}")
+# TODO: Add a new category... Will revisit this later
+@app.get("/add_category/{category_name}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
