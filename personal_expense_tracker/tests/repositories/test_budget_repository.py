17c4603a783720ccc0125cd87df20d859b826214
nlutala <nathanlutala@outlook.com>
2025-05-16 13:34:30 +0100

    Add test files for the repository classes

---
+++ personal_expense_tracker/tests/repositories/test_budget_repository.py
@@ -0,0 +1,24 @@
+"""
+Tests for all the methods in the BudgetRepository class.
+"""
+from personal_expense_tracker.repositories.budget import BudgetRepository
+
+
+def test_budget_init():
+    pass
+
+
+def test_get_budget():
+    pass
+
+
+def test_get_expenditure():
+    pass
+
+
+def test_get_remaining():
+    pass
+
+
+def test_update_budget():
+    pass
